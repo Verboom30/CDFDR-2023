5de5231366214959c5a9dae14657f0d431d76aed
Clement <56961627+Verboom30@users.noreply.github.com>
2023-07-09 18:29:15 +0200

    Control speed stepper moteur with XBOX controller

--- 3_SW/CDFDR/src/main.cpp
+++ 3_SW/CDFDR/src/main.cpp
@@ -4,7 +4,7 @@
 #include "tim.h"
 #include "develop_ticker.hpp"
 #include <string.h>
-#define MAXIMUM_BUFFER_SIZE  32
+#define MAXIMUM_BUFFER_SIZE  128
 #define COEFF  0.01
 #define LO  0//0.132 //132 cm
 #define SPEED  20000 // max 50000 Mstepper 16 3200Ma
@@ -16,13 +16,9 @@ BufferedSerial uart(D1, D0,115200);
 Stepper StepperB(STEP_M2, DIR_M2);
 Stepper StepperC(STEP_M3, DIR_M3);
 TestTicker test_ticker(STEP_M1);
+TestTicker test_ticker1(STEP_M2);
+TestTicker test_ticker2(STEP_M3);
 
-// DigitalOut dirM1(DIR_M1);
-// DigitalOut dirM2(DIR_M2);
-// DigitalOut dirM3(DIR_M3);
-// DigitalOut StpM1(STEP_M1);
-// DigitalOut StpM2(STEP_M2);
-// DigitalOut StpM3(STEP_M3);
 DigitalOut pc_activity(LED1);
 DigitalOut uart_activity(LED2);
 AnalogIn   Poten(A0);
@@ -39,32 +35,40 @@ float Vb=0;
 float Vc=0;
 int Vx=0;
 int Vy=0;
+int Vx2=0;
+int Vy2=0;
 float percentage =0;
+
+
+void print(const std::string &str) {
+    pc.write(str.c_str(), str.length());
+}
+
 void conCharReceived(void)
 {
-     char buffer[MAXIMUM_BUFFER_SIZE] = {0};
-     while(1){
-        uart.read(buffer, sizeof(buffer));
-        uart_activity = !uart_activity;
+  const char * separators = "\t\n";
+  char buffer[MAXIMUM_BUFFER_SIZE] = {0};
+  while (1) {
+    if (uint32_t num = uart.read(buffer, sizeof(buffer))) {
+      uart_activity = !uart_activity;
+      //pc.write(buffer,num);
 
-        sscanf(buffer,"Y: %d",&Vy); 
-        sscanf(buffer,"X: %d",&Vx); 
-
-        //if((12000>Vx) and (Vx>-12000)) Vx =0;
-        //if((12000>Vy) and (Vy>-12000)) Vy =0;
-
-        if(strstr(buffer,"A")){
-            Vy =0;   
-            Vx =0; 
-            //StepperA.stop();
-            //StepperB.stop();
-            //StepperC.stop();
-            //StepperA.stop();
-            //StepperA.setSpeed(10000);
-        }                
-        //printf("X==>%d Y==>%d\n",Vx,Vy);
-     }
+      char * strToken = strtok ( buffer, separators );
+      while ( strToken != NULL ) {
+          sscanf(strToken,"LeftHatY: %d",&Vy); 
+          sscanf(strToken,"LeftHatX: %d",&Vx); 
+          printf("X==>%d Y==>%d\n",Vx,Vy);
+          // On demande le token suivant.
+          strToken = strtok ( NULL, separators );
+      }    
+    }   
+    if(Vx>1000 or Vx<-1000)Vx2 =Vx;
+    if(Vy>1000 or Vy<-1000)Vy2 =Vy;     
+  }  
+    
 }
+
+
 // void omni_move_xbox(const int Vx,const int Vy, float *Va, float *Vb, float *Vc)
 // {
 //     // *Va = ((-1/2)*Vx + (sqrt(3)/2)*Vy +LO)*COEFF;
@@ -99,8 +103,7 @@ void conCharReceived(void)
 void show_thread(void){
     while (1)
     {
-        
-        printf("poten: %3.3f%%, %f\n",percentage, 15000*(percentage/100));
+        printf("poten: %3.3f%%, %f\n",percentage, 20000*(percentage/100));
     }
 }
 
@@ -109,7 +112,22 @@ void stepperA(void){
    
     while (1)
     {
-         test_ticker.setSpeed(15000*(percentage/100));
+         test_ticker.setSpeed(Vx2);
+    }
+}
+
+void stepperB(void){
+   
+    while (1)
+    {
+         test_ticker1.setSpeed(20000*(percentage/100));
+    }
+}
+void stepperC(void){
+   
+    while (1)
+    {
+         test_ticker2.setSpeed(40000*(percentage/100));
     }
 }
 
@@ -129,11 +147,17 @@ int main()
 
 
 
-    //serial_thread.start(conCharReceived);
+    serial_thread.start(conCharReceived);
     // //time_upA.attach(stepperA,Te);
     test_ticker.setSpeed(2000);
     test_ticker.run();
+    // test_ticker1.setSpeed(2000);
+    // test_ticker1.run();
+    // test_ticker2.setSpeed(2000);
+    // test_ticker2.run();
     StepperA_thread.start(stepperA);
+    // StepperB_thread.start(stepperB);
+    // StepperC_thread.start(stepperC);
 
 
     // printf("INIT_Timer\n");
@@ -143,14 +167,14 @@ int main()
     // HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
     // htim1.Instance->ARR = 2;
     // htim1.Instance->CCR1 = 2/2;
-    serial_thread.start(show_thread);
+    //serial_thread.start(show_thread);
  
 
    
    
     while (1)
     {
-        percentage = Poten.read()*100.0f;
+        //percentage = Poten.read()*100.0f;
         //omni_move_xbox(Vx,Vy,&Va,&Vb,&Vc);
         
         //wait_us(1000);
