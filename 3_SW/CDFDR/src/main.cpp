5c5a44392d32a0025ae8109b93e9dcfbdbcae8c7
Clement <56961627+Verboom30@users.noreply.github.com>
2023-07-14 17:46:10 +0200

    Robot Xbox move OK

--- 3_SW/CDFDR/src/main.cpp
+++ 3_SW/CDFDR/src/main.cpp
@@ -4,23 +4,28 @@
 #include "tim.h"
 #include "develop_ticker.hpp"
 #include <string.h>
+#include <math.h>
 #define MAXIMUM_BUFFER_SIZE  128
-#define COEFF  1
-#define LO  0//0.132 //132 cm
+#define COEFF  0.1
 #define SPEED  20000 // max 50000 Mstepper 16 3200Ma
 #define DIS    100000
+#define PI 3.14159265
 BufferedSerial pc(USBTX, USBRX,115200);
 BufferedSerial uart(D1, D0,115200);
 
 //Stepper StepperA(STEP_M1, DIR_M1);
-Stepper StepperB(STEP_M2, DIR_M2);
-Stepper StepperC(STEP_M3, DIR_M3);
+//Stepper StepperB(STEP_M2, DIR_M2);
+//Stepper StepperC(STEP_M3, DIR_M3);
 TestTicker test_ticker(STEP_M1);
 TestTicker test_ticker1(STEP_M2);
 TestTicker test_ticker2(STEP_M3);
 
 DigitalOut pc_activity(LED1);
 DigitalOut uart_activity(LED2);
+
+DigitalOut M1(DIR_M1);
+DigitalOut M2(DIR_M2);
+DigitalOut M3(DIR_M3);
 AnalogIn   Poten(A0);
 Thread serial_thread;
 Thread StepperA_thread;
@@ -30,13 +35,15 @@ Ticker time_upA;
 Ticker time_upB;
 Ticker time_upC;
 float Te=0.01;
-float Va=0;
-float Vb=0;
-float Vc=0;
+float V1=0;
+float V2=0;
+float V3=0;
 int Vx=0;
 int Vy=0;
 int Vx2=0;
 int Vy2=0;
+float R = 0.136; // robot wheel-base radius
+float theta_dot = 0; // robot wheel-base radius
 float percentage =0;
 
 
@@ -58,8 +65,9 @@ void conCharReceived(void)
           sscanf(strToken,"LeftHatY: %d",&Vy); 
           sscanf(strToken,"LeftHatX: %d",&Vx); 
           if(Vx>1000 or Vx<-1000)Vx2 =Vx;
-          if(Vy>1000 or Vy<-1000)Vy2 =Vy;   
-          printf("X==>%d Y==>%d\n",Vx2,Vy2);
+          if(Vy>1000 or Vy<-1000)Vy2 =-Vy;   
+          //printf("X==>%d Y==>%d\n",Vx2,Vy2);
+          printf("V1:%f V2:%f V3:%f\n",V1,V2,V3);
           // On demande le token suivant.
           strToken = strtok ( NULL, separators );
       }    
@@ -69,11 +77,24 @@ void conCharReceived(void)
     
 }
 
-void omni_move_xbox(const int Vx,const int Vy, float *Va, float *Vb, float *Vc)
+void omni_move_xbox(const int Vx,const int Vy, float *V1, float *V2, float *V3)
 {
-    *Va = ((-1/2)*Vx + (sqrt(3)/2)*Vy +LO)*COEFF;
-    *Vb = ((-1/2)*Vx - (sqrt(3)/2)*Vy +LO)*COEFF;
-    *Vc = (Vx + LO)*COEFF;
+    //*V1 = ((-1/2)*Vx - (sqrt(3)/2)*Vy +LO)*COEFF;
+    //*V3 = ((-1/2)*Vx + (sqrt(3)/2)*Vy +LO)*COEFF;
+    //*V2 = (Vx + LO)*COEFF;
+
+   *V2 = (-R*theta_dot + Vx)*COEFF ;
+   *V1 = (-R*theta_dot - 0.5*Vx - sin(PI/3.0)*Vy)*COEFF;
+   *V3 = (-R*theta_dot - 0.5*Vx + sin(PI/3.0)*Vy)*COEFF;
+
+    
+
+    // *V1 = ((sqrt(pow(Vx,2)+pow(Vy,2)))*(sin(acos((Vx)/(sqrt(pow(Vx,2)+pow(Vy,2)))) - 90)))*COEFF;
+
+    // *V2 = ((sqrt(pow(Vx,2)+pow(Vy,2)))*(sin(acos((Vx)/(sqrt(pow(Vx,2)+pow(Vy,2)))) + 30)))*COEFF;
+
+    // *V3 = ((sqrt(pow(Vx,2)+pow(Vy,2)))*(sin(acos((Vx)/(sqrt(pow(Vx,2)+pow(Vy,2)))) + 150)))*COEFF;
+
 }
 
 
@@ -106,27 +127,28 @@ void show_thread(void){
 
 
 void stepperA(void){
-   
+    test_ticker.setSpeed(2000);
+    test_ticker.run();
     while (1)
     {
-         test_ticker.setSpeed(Va);
+         test_ticker.setSpeed((int)V1);
     }
 }
 
 void stepperB(void){
-   
+    test_ticker1.setSpeed(2000);
+    test_ticker1.run();
     while (1)
     {
-         //test_ticker1.setSpeed(20000*(percentage/100));
-         test_ticker1.setSpeed(Vb);
+         test_ticker1.setSpeed((int)V2);
     }
 }
 void stepperC(void){
-   
+    test_ticker2.setSpeed(2000);
+    test_ticker2.run();
     while (1)
     {
-         //test_ticker2.setSpeed(40000*(percentage/100));
-         test_ticker2.setSpeed(Vc);
+         test_ticker2.setSpeed((int)V3);
     }
 }
 
@@ -148,12 +170,11 @@ int main()
 
     serial_thread.start(conCharReceived);
     // //time_upA.attach(stepperA,Te);
-    test_ticker.setSpeed(2000);
-    test_ticker.run();
-    test_ticker1.setSpeed(2000);
-    test_ticker1.run();
-    test_ticker2.setSpeed(2000);
-    test_ticker2.run();
+    
+    //test_ticker1.setSpeed(2000);
+    //test_ticker1.run();
+    //test_ticker2.setSpeed(2000);
+    //test_ticker2.run();
     StepperA_thread.start(stepperA);
     StepperB_thread.start(stepperB);
     StepperC_thread.start(stepperC);
@@ -174,11 +195,33 @@ int main()
     while (1)
     {
         //percentage = Poten.read()*100.0f;
-        omni_move_xbox(Vx2,Vy2,&Va,&Vb,&Vc);
+        omni_move_xbox(Vx2,Vy2,&V1,&V2,&V3);
+        if(V1>0){
+             M1 =1;
+        }else{
+            
+            M1 =0;
+        }
+          if(V2>0){
+             M2 =1;
+        }else{
+            
+            M2 =0;
+        }
+          if(V3>0){
+             M3 =1;
+        }else{
+            
+            M3 =0;
+        }
+           
+        
+        
+       
         
         //wait_us(1000);
         //n=n+1;
-        //printf("Va:%f Vd:%f Vc:%f\n",Va,Vb,Vc);
+      
         //htim1.Instance->PSC =abs(Vc);
         
       
