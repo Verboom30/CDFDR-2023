3ebf8bb8abd6cb92a23b8cfab36c9f3e25445d8f
Clement <56961627+Verboom30@users.noreply.github.com>
2024-03-26 22:33:28 +0100

    Lidar hex

--- 3_SW/CDFDR/lib/LIDAR/lidar.cpp
+++ 3_SW/CDFDR/lib/LIDAR/lidar.cpp
@@ -32,28 +32,39 @@ void on_rx_interrupt(void)
 //                          Protected Methods                           //
 //***********************************/************************************
 
+
+  uint32_t charToUInt32(const char* src) {
+    uint32_t ret = 0;
+    char* dst = (char*)&ret;
+    for(unsigned i = 0; (i < sizeof(src)) && (*src); ++i, ++src)
+      dst[i] = *src;
+
+    return ret;
+  }
+
+
 void Lidar::routine_lidar(void)
 {
     char buffer[DATA_PACKET_SIZE*2] = {0};
+    char buffer_hex[DATA_PACKET_SIZE*2] = {0};
     //BufferedSerial pc(USBTX, USBRX,230400);
-    //const char * separators = (const char *)(HEADER);
     while(1){
       
         if (readable()) {
-       
-      
-            //int dataPacket = 0;
-            if (uint32_t num = read(buffer, sizeof(buffer))) {
-                //*DataPacket = "TEST";//strtok(buffer, separators);
-                //pc.write(buffer,num);
-                printf("%s\n",buffer);
-                //printf("DataLidar: %x\n",strtok(buffer, separators));
-            
-                //sscanf(strToken,"LeftHatY: %x",&dataPacket);
-                //return dataPacket;
-                }
+          //int dataPacket = 0;
+          if (uint32_t num = read(buffer, sizeof(buffer))) {
+
+            printf("\nbuffer_hex: ");
+            for (uint32_t i = 0; i < num; i++)
+            {
+                memcpy(&buffer_hex[i], buffer+i, 1);
+                printf("%x",buffer_hex[i]);
+            }
+          }
+          
         }else{
-            sync();
+          sync();
+            
             //printf("LIDAR\n");
            
         }
