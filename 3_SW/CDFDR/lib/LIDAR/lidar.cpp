b8b8db39c8361782e408252c9c6d57de4cd0c436
Clement <56961627+Verboom30@users.noreply.github.com>
2024-03-26 23:59:53 +0100

    Use LiDARFrameTypeDef

--- 3_SW/CDFDR/lib/LIDAR/lidar.cpp
+++ 3_SW/CDFDR/lib/LIDAR/lidar.cpp
@@ -12,14 +12,42 @@ Lidar::Lidar(PinName tx, PinName rx, int baud) : BufferedSerial(tx, rx, baud)
 //***********************************/************************************
 //                                Get Set                               //
 //***********************************/************************************
-void Lidar::ReadLidar(char** DataPacket)
+void Lidar::ReadLidar(void)
 {
-   
-}
+  uint8_t buffer[DATA_PACKET_SIZE*2] = {0};
+  uint8_t DataPacket[DATA_PACKET_SIZE] = {0};
+  if (readable()) {
+    if (uint32_t num = read(buffer, sizeof(buffer))) {
 
-void on_rx_interrupt(void)
-{
-    
+      // printf("\nbuffer_hex: ");
+      // for (uint32_t i = 0; i < num; i++)
+      // {
+      //     printf("%x",buffer[i]);
+      // }
+      uint8_t index;
+      for (index = 0; index < DATA_PACKET_SIZE-1 && (buffer[index] != HEADER || buffer[index+1] != VERLEN); index++);
+      printf("\nDATA_PACKET: ");
+      for (uint8_t i = index; i < DATA_PACKET_SIZE+index; i++)
+      {
+        printf("%02X",buffer[i]);
+        DataPacket[i] = buffer[i];
+      }
+      _dataPacket.header                = DataPacket[0];
+      _dataPacket.ver_len               = DataPacket[1];
+      _dataPacket.speed                 = DataPacket[2] & DataPacket[3];
+      _dataPacket.start_angle           = DataPacket[4] & DataPacket[5];
+
+      for (uint8_t i = 0; i < POINT_PER_PACK; i++)
+      {
+        _dataPacket.point[i].distance   = DataPacket[6+3*i] & DataPacket[7+3*i];
+        _dataPacket.point[i].intensity  = DataPacket[8+3*i];
+      }
+
+      _dataPacket.end_angle             = DataPacket[42] & DataPacket[43];
+      _dataPacket.timestamp             = DataPacket[44] & DataPacket[45];
+      _dataPacket.crc8                  = DataPacket[46];
+    }
+  }
 }
 
 
@@ -45,37 +73,7 @@ void on_rx_interrupt(void)
 
 void Lidar::routine_lidar(void)
 {
-    uint8_t buffer[DATA_PACKET_SIZE*2] = {0};
     while(1){
-        if (readable()) {
-          if (uint32_t num = read(buffer, sizeof(buffer))) {
-
-            // printf("\nbuffer_hex: ");
-            // for (uint32_t i = 0; i < num; i++)
-            // {
-            //     printf("%x",buffer[i]);
-            // }
-            uint8_t index;
-            for (index = 0; index < DATA_PACKET_SIZE && buffer[index] != HEADER; index++);
-            printf("\nDATA_PACKET: ");
-            for (uint8_t i = index; i < DATA_PACKET_SIZE+index; i++)
-            {
-               printf("%02X",buffer[i]);
-            }
-            
-          
-            
-
-
-
-          
-          }
-          
-        }else{
-          sync();
-            
-            //printf("LIDAR\n");
-           
-        }
+      ReadLidar();
     }
 }
